0451bb3babf235a0cf4b8e98905d4765cafe61ef
vicentecrespop <vicentecrespo45@gmail.com>
2022-11-09 16:45:55 -0300

    src updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Footer from "./components/Footer";
+import AccountModal from "./components/AccountModal";
+import './App.scss';
+
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Dashboard from "./views/Dashboard";
+
+const PrivateRoute = ({ children, isLogged, ...rest }) => {
+  return isLogged ? children : <Navigate to="/login"/>
+}
 
 function App() {
+  const [ showModal, setShowModal ] = useState(false)
+  const [ name, setName ] = useState()
+  const [ account, setAccount ] = useState()
+  const isLogged = name && account
+  
+  const fakeAuth = {
+    login(name, account, cb) {
+      setName(name)
+      setAccount(account)
+      setTimeout(cb, 100)
+    },
+    logout(cb) {
+      setName()
+      setAccount()
+      setTimeout(cb, 100)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar onClick={() => setShowModal(true)} isLogged={isLogged} auth={fakeAuth}/>
+      <Routes>
+        <Route path="/" element={<Home handleClick={() => setShowModal(true)}/>} />
+        <Route path="/login" element={<Login auth={fakeAuth}/>} />
+        <Route path="/dashboard/*" element={<PrivateRoute isLogged={isLogged}><Dashboard name={name} account={account} /></PrivateRoute>} />
+      </Routes>
+      <Footer />
+      <AccountModal show={showModal} handleClose={() => setShowModal(false)} auth={fakeAuth}/>
+    </Router>
   );
 }
 
