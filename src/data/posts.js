29a0ca817eed6c4c6d3a3c3a889acb12688d01c4
vicentecrespop <vicentecrespo45@gmail.com>
2022-11-09 16:44:41 -0300

    data

---
+++ src/data/posts.js
@@ -0,0 +1,30 @@
+import cardImg1 from '../assets/card-img1.svg'
+import cardImg2 from '../assets/card-img2.svg'
+import cardImg3 from '../assets/card-img3.svg'
+
+
+const posts = [
+    {
+        id: 1,
+        image: cardImg1,
+        title: 'Lorem ipsum dolor sit',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue.',
+        action: 'Peça já'
+    },
+    {
+        id: 2,
+        image: cardImg2,
+        title: 'Phasellus eget elementum diam',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue.',
+        action: 'Peça já'
+    },
+    {
+        id: 3,
+        image: cardImg3,
+        title: 'Fusce tempus',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue.',
+        action: 'Peça já'
+    },
+]
+
+export default posts
